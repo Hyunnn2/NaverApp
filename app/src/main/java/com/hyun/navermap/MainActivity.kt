1bd1779c782023e2acdede9e6e87386aaa0ee4df
Hyunnn2 <gusrud5015@gmail.com>
2023-09-02 16:24:01 +0900

    좌표

--- app/src/main/java/com/hyun/navermap/MainActivity.kt
+++ app/src/main/java/com/hyun/navermap/MainActivity.kt
@@ -2,10 +2,20 @@ package com.hyun.navermap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.naver.maps.geometry.LatLng
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val coord = LatLng(37.5670135, 126.9783740)
+
+        Toast.makeText(this,
+            "위도: ${coord.latitude}, 경도: ${coord.longitude}",
+            Toast.LENGTH_SHORT).show()
+
     }
+
 }
