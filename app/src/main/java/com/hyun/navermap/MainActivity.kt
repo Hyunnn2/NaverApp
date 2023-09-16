f2ca939338af8e5b974c593531377a612fe9d39c
Hyunnn2 <gusrud5015@gmail.com>
2023-09-17 03:16:45 +0900

    코드 정리 / 주석 달기 / 정보창 크기 조정

--- app/src/main/java/com/hyun/navermap/MainActivity.kt
+++ app/src/main/java/com/hyun/navermap/MainActivity.kt
@@ -1,20 +1,12 @@
 package com.hyun.navermap
 
-import android.content.Context
-import android.content.res.Resources
-import android.graphics.Color
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.annotation.NonNull
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraPosition
 import com.naver.maps.map.LocationTrackingMode
@@ -26,9 +18,6 @@ import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.FusedLocationSource
 import com.naver.maps.map.util.MarkerIcons
 import kotlinx.coroutines.Runnable
-import kotlinx.coroutines.delay
-import org.json.JSONArray
-import org.json.JSONException
 import org.json.JSONObject
 import java.io.InputStream
 import java.util.Calendar
@@ -43,10 +32,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var timerText: String
     private lateinit var handler: Handler
-
-    //마커
-    private val marker = Marker()
-    private val marker1 = Marker()
 
     private lateinit var infoWindow: InfoWindow
     private var timervalue: Int = 10
@@ -72,10 +57,10 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         val inputStream: InputStream = resources.openRawResource(rawResourceId)
         val jsonString = inputStream.bufferedReader().use { it.readText() }
 
-        // JSON 데이터 파싱
+        // JSON 데이터 파싱 jsonObject이용
         val jsonObject = JSONObject(jsonString)
 
-        // 0부터 69까지의 데이터 가져오기
+        // 0부터 69까지의 신호등 데이터 가져오기
         for (i in 0 until 70) {
             val signalObject = jsonObject.getJSONObject("Signal").getJSONObject(i.toString())
             val signalData = Signal(
@@ -86,14 +71,15 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 etc = signalObject.getString("etc"),
                 time = parseTimeInfo(signalObject.getJSONObject("time"))
             )
-            signalDataList.add(signalData)
+            signalDataList.add(signalData) // signalDataList에 0부터 69까지의 데이터가 저장
         }
 
-        // signalDataList에 0부터 69까지의 데이터가 저장
     }
 
     private fun parseTimeInfo(timeObject: JSONObject): Map<String, TimeInfo> {
         val timeInfoMap = mutableMapOf<String, TimeInfo>()
+
+        // 0부터 69까지의 신호등 데이터 가져오기(time)
         for (key in timeObject.keys()) {
             val timeInfoObject = timeObject.getJSONObject(key)
             val timeInfo = TimeInfo(
@@ -112,21 +98,20 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         //초기 위치 : 경상대 정문
         val cameraPosition = CameraPosition(
-            LatLng(35.1524, 128.1049),
-            17.0
+            LatLng(35.152391, 128.105066), 18.0
         )
         naverMap.cameraPosition = cameraPosition
 
-        //대중교통
+        //대중교통 아이콘 띄우기
         naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_TRANSIT, true)
 
-        this.naverMap = naverMap
         //위치추적
+        this.naverMap = naverMap
         naverMap.locationSource = locationSource
-
         val uiSettings = naverMap.uiSettings
         uiSettings.isLocationButtonEnabled = true
         naverMap.locationTrackingMode = LocationTrackingMode.None //실시간 위치 추적 모드
+
 
         // 현재 시간 가져오기
         val currentTime = Calendar.getInstance()
@@ -140,23 +125,24 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             else -> "21:00"
         }
 
-        // signalDataList에 저장된 데이터를 사용하여 마커를 추가하는 부분을 추가
+        // signalDataList에 저장된 데이터를 사용하여 마커추가
         for (signalData in signalDataList) {
 
             // 현재 시간에 해당하는 데이터를 찾기
             val timeInfo = signalData.time[timeKey]
 
+            //마커 찍기
             val marker = Marker()
             marker.position = LatLng(signalData.latitude, signalData.longitude)
             marker.captionText = signalData.captionText
             marker.icon = MarkerIcons.BLACK // 원하는 마커 아이콘 설정
             marker.map = naverMap
 
-            //정보창
-            val infoWindow = InfoWindow()
             //현재 시간에 따른 onTime
             val timerText = "${timeInfo?.onTime}초"
 
+            //정보창
+            val infoWindow = InfoWindow()
             infoWindow.adapter = object : InfoWindow.DefaultViewAdapter(applicationContext) {
                 override fun getContentView(p0: InfoWindow): View {
                     val view = layoutInflater.inflate(R.layout.activity_info, null)
@@ -173,10 +159,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 }
             }
 
-            marker.position = LatLng(signalData.latitude, signalData.longitude)
-            marker.captionText = signalData.captionText
-            marker.map = naverMap
-
+            //마커 클릭 이벤트
             marker.setOnClickListener {
                 if (infoWindow.map == null) {
                     infoWindow.open(marker)
@@ -185,8 +168,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 }
                 true
             }
-        }
 
+        }
 
 
         /*        //마커 : 경상대 후문 이노티
