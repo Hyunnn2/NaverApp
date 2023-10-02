f4ff661e660906c17afe6b93405428234582d87b
KIM-JANG-BAE <rlawkdqo159@naver.com>
2023-10-02 14:48:01 +0900

    TimerInfo 클래스 생성 및 메인 클래스에 불필요한 코드 제거

--- app/src/main/java/com/hyun/navermap/MainActivity.kt
+++ app/src/main/java/com/hyun/navermap/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Handler
 import android.view.View
 import android.widget.TextView
 import androidx.annotation.NonNull
+import com.hyun.navermap.InfoTimer.InfoTimer
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraPosition
 import com.naver.maps.map.LocationTrackingMode
@@ -154,31 +155,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             val infoWindow = InfoWindow()
             infowindowList.add(infoWindow)
 
-            //현재 시간에 따른 onTime
-            var timerText = "${timeInfo?.onTime}초"
-
-            //현지 시간에 따른 onTime -1연산을 위한 변수 설정
-            var timervar : Int = timeInfo?.onTime?.toInt() ?: 0
-
-            //각 마커마다의 timerRunnable 생성(해당 함수는 마커의 정보창,신호등 이름, 온타임을 인자로 받아서처리 추가 처리 하고싶은 내용 있으면 말하길)
-            val timerRunnable = CreatetimerRunnable(infoWindow, timervar, signalData.No)
-
-            infoWindow.adapter = object : InfoWindow.DefaultViewAdapter(applicationContext) {
-                override fun getContentView(p0: InfoWindow): View {
-
-                    val view = layoutInflater.inflate(R.layout.activity_info, null)
-                    val timerTextView = view.findViewById<TextView>(R.id.Timer_cross)
-                    val titleText = view.findViewById<TextView>(R.id.Name_cross)
-
-                    titleText.text = signalData.No
-                    timerTextView.text = timerText
-
-                    handler = Handler()
-                    handler.postDelayed(timerRunnable, 1000)
-
-                    return view
-                }
-            }
+            val infoTimer = InfoTimer(marker, timeInfo, signalData,applicationContext ,infoWindow)
 
             //마커 클릭 이벤트
             marker.setOnClickListener {
@@ -229,35 +206,4 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onLowMemory()
         mapView.onLowMemory()
     }
-
-    private fun CreatetimerRunnable(infoWindow : InfoWindow, timervar : Int, captiontext : String): Runnable{
-        return object : Runnable {
-            var timervar_a = timervar - 1
-
-            override fun run() {
-                if (timervar_a >= 0) {
-                    timerText = "남은 시간: ${timervar_a} 초"
-                    infoWindow.adapter = object : InfoWindow.DefaultViewAdapter(applicationContext) {
-                        override fun getContentView(infoWindow: InfoWindow): View {
-
-                            val view = layoutInflater.inflate(R.layout.activity_info, null)
-                            val titleText = view.findViewById<TextView>(R.id.Name_cross)
-                            titleText.text = captiontext
-
-                            val timerTextView = view.findViewById<TextView>(R.id.Timer_cross)
-                            timerTextView.text = timerText
-                            return view
-                        }
-                    }
-                    timervar_a--
-                    handler.postDelayed(this, 1000) // 1초마다 업데이트
-                } else {
-                    infoWindow.close()
-                }
-            }
-        }
-    }
-
-
-
 }
